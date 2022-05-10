f54b0ebb0086efa90e7e6827fe3717f95066aea0
drsem <drsem@DANIIL>
2022-05-10 18:12:19 +0400

    Добавьте файлы проекта.

---
+++ config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoreRandomBot
+{
+    public static class configToken
+    {
+        public static string Token { get; set; } = "5137768860:AAFTqyfNolBG0xBSvHY4sS8nwAIH1w6-Fi4";
+    }
+}
